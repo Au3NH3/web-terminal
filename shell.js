ba79f95c42fd57143c18c60f3acbdcf285795e05
andy <a@a>
2023-12-31 20:23:10 +0800

    fix warns/errs on edge; add command: history and clear-screen

--- shell.js
+++ shell.js
@@ -1,5 +1,6 @@
 // ----------
 // author: https://github.com/au3nh3
+// last modify: 2023/12/31 19:15
 // ----------
 
 const outputs = document.querySelector('#outputs')
@@ -7,88 +8,100 @@ const form_cmd = document.querySelector('#form_cmd')
 const  prompt = document.querySelector('#prompt')
 const  input = document.querySelector('#input')
 
-var MAX_CMD_NUM = 15
-var MAX_HIST_NUM = 20
+var MAX_CMD_NUM = 5
+var MAX_HIST_NUM = 8
 var prompt_str = 'test@cmd$ '
 //var cmd_in = document.createElement('span')
 var prompt_span = '<span class="prompt">' + prompt_str + '</span>'
+var prompt_div = '<div class="prompt">' + prompt_str + '</div>'
 var banner_str = ' <div>Welcome! This is a testing page</div>'
 var nav_str = " <div>go to <a href='/index.html'>home</a></div>"
 var ncmd = 0
 var hist_cmd = []
 var hist_index = 0   
 
-//form_cmd.prepend(prompt_span)
 prompt.innerHTML = prompt_str
-//form_cmd.innerHTML = prompt_span
 outputs.innerHTML = banner_str + nav_str
-//label_prompt.innerHTML = prompt_str
 
 input.focus()
 document.addEventListener('click', (event)=>{
-  event_element = event.target.tagName
-  if (event_element == 'HTML') {
-	input.focus()
-  }
-  //console.log(event.target.tagName)
+	event_element = event.target.tagName
+	if (event_element == 'HTML') {
+		input.focus()
+	}
+	//console.log(event.target.tagName)
 })
 
 var curr_input = ''
 input.addEventListener('keyup', (event)=>{
-  let key = event.key
-  //let curr_input = input.value
-  if (key == 'ArrowUp') {
-	//if (hist_index == hist_cmd.length) {
-	//  curr_input = input.value
+	let key = event.key
+	if (key == 'ArrowUp') {
+		//if (hist_index == hist_cmd.length) {
+		//  curr_input = input.value
+		//}
+		if (hist_index > 0) {
+			input.value = hist_cmd[--hist_index]
+		}
+	}
+	else if (key == 'ArrowDown') {
+		if (hist_index == hist_cmd.length-1 && input.value){
+		input.value = curr_input
+		hist_index++
+		}
+		else if (hist_index < hist_cmd.length-1) {
+			input.value = hist_cmd[++hist_index]
+		}
+	}
+	//else {
+	//  var curr_input = input.value
 	//}
-	if (hist_index > 0) {
-	  input.value = hist_cmd[--hist_index]
-	}
-  }
-  else if (key == 'ArrowDown') {
-	if (hist_index == hist_cmd.length-1 && input.value){
-	  input.value = curr_input
-	  hist_index++
-	}
-	else if (hist_index < hist_cmd.length-1) {
-	  input.value = hist_cmd[++hist_index]
-	}
-  }
-  //else {
-  //  var curr_input = input.value
-  //}
-  //console.log(hist_index, hist_cmd.length, curr_input)
+	//console.log(hist_index, hist_cmd.length, curr_input)
 })
 
 function request_cmd(cmd) {
-  return 'your input cmd is: ' + cmd
+	return 'your input cmd is: ' + cmd
 }
 form_cmd.addEventListener('submit', (event)=>{
-  event.preventDefault()
-  let cmd = form_cmd.input.value.trim()
-  hist_index = hist_cmd.length
-  if (cmd && cmd != hist_cmd[hist_index-1]) {
-	if (hist_index >= MAX_HIST_NUM) {
-	  hist_cmd.shift()
-	  hist_index--
+	event.preventDefault()
+	let cmd = input.value.trim()
+	hist_index = hist_cmd.length
+	let div_in  = document.createElement('div')
+	div_in.innerHTML = prompt_span + input.value
+	if (ncmd >= MAX_CMD_NUM) {
+		outputs.innerHTML = nav_str
+		ncmd = 0
 	}
-	hist_cmd[hist_index++] = cmd
-  }
-  if (ncmd >= MAX_CMD_NUM) {
-	outputs.innerHTML = ''
-	ncmd = 0
-	if (!cmd) {return}
-  }
-  let div_in  = document.createElement('div')
-  let div_out = document.createElement('div')
-  //div_out.setAttribute("class", "out")
-  let res = ' '
-  if (cmd) {
-	res += request_cmd(cmd)
-  }
-  div_in.innerHTML = prompt_span + form_cmd.input.value
-  outputs.appendChild(div_in)
-  div_out.innerHTML = res
-  outputs.appendChild(div_out)
-  ncmd++
+	outputs.appendChild(div_in)
+	if (!cmd) {
+		ncmd += 0.5
+		console.log(ncmd)
+		return
+	}
+	if (cmd != hist_cmd[hist_index-1]) {
+		if (hist_index >= MAX_HIST_NUM) {
+		hist_cmd.shift()
+		hist_index--
+		}
+		hist_cmd[hist_index++] = cmd
+	}
+	let res = ''
+	if (cmd == 'clear' || cmd == 'cls') {
+		outputs.innerHTML = nav_str
+		ncmd = 0
+	}
+	else if (cmd == 'hist') {
+		for (index in hist_cmd) {
+		  res += index + '\t\t' + hist_cmd[index] + '\n'
+		}
+	}
+	else {
+		res = request_cmd(cmd)
+	}
+	let div_out = document.createElement('div')
+	div_out.setAttribute("class", "out")
+	div_out.innerHTML = res
+	outputs.appendChild(div_out)
+	input.value = ''
+	ncmd++
+	console.log(ncmd)
 })
